81651e1a614ca7c6324f2e46bb0781d1aa7be3d0
Swapnil Ingle <1985swapnil@gmail.com>
2018-02-22 10:44:48 +0000

    Add hide/unhide/block/unblock interface in the tool
    Signed-off-by: Swapnil Ingle <1985swapnil@gmail.com>

--- tool/wrapfs.h
+++ tool/wrapfs.h
@@ -5,7 +5,9 @@
 
 #define WRAPFS_IOC_HIDE         _IO('h', 1)
 #define WRAPFS_IOC_UNHIDE       _IO('h', 2)
-#define WRAPFS_IOC_HIDE_LIST	_IO('h', 3)
+#define WRAPFS_IOC_BLOCK	_IO('h', 3)
+#define WRAPFS_IOC_UNBLOCK	_IO('h', 4)
+#define WRAPFS_IOC_HIDE_LIST	_IO('h', 5)
 
 struct wrapfs_misc_ioctl {
 	unsigned long sz;
--- tool/wrapfsctl.c
+++ tool/wrapfsctl.c
@@ -6,43 +6,69 @@
 #include <errno.h>
 #include <getopt.h>
 #include <string.h>
-#include <sys/types.h>
+#include <linux/types.h>
+#include <limits.h>
 
 #include "wrapfs.h"
+#define ARRAY_SIZE(arr) (sizeof(arr)/sizeof((arr)[0]))
 
-long cmd;
+const char *progname;
 
-void usage(char **argv)
+static int hide_file(char **args);
+static int unhide_file(char **args);
+static int block_file(char **args);
+static int unblock_file(char **args);
+static int list_all(char **args);
+static int help(char **args);
+
+struct cmd_opts {
+	const char *cmd;
+	int (*func) (char **args);
+	const char *usage;
+} cmds[] = {
+	{"hide",	hide_file,	"hide     <file|directory>"},
+	{"unhide",	unhide_file,	"unhide   <file|directory>"},
+	{"block",	block_file,	"block    <file|directory>"},
+	{"unblock",	unblock_file,	"unblock  <file|directory>"},
+	{"list",	list_all,	"list"},
+	{"help",	help,		"help"},
+};
+
+void usage()
 {
-	printf("Usage: %s [options]\n", argv[0]);
-	printf("\toptions:\n");
-	printf("\t\t-h|--hide\t[file|directory]\n");
-	printf("\t\t-u|--unhide\t[file|directory]\n");
-	printf("\t\t-l|--list\t\n");
+	int i;
+
+	printf("  %s [options]\n", progname);
+	for (i=0; i<ARRAY_SIZE(cmds); i++)
+		printf("\t\t%s\n", cmds[i].usage);
 }
 
-static int do_ioctl(const char *fname)
+static int get_inode_number(const char *path, unsigned long *ino)
 {
-	struct wrapfs_misc_ioctl wr_ioctl = {0};
 	struct stat stbuf;
-	int fd, err;
+	int err;
 
-	err = stat(fname, &stbuf);
+	err = stat(path, &stbuf);
 	if (err) {
-		printf("stat failed on %s: %s\n", fname, strerror(-err));
+		printf("stat failed on %s: %s\n", path, strerror(errno));
 		return err;
 	}
-	strcpy(wr_ioctl.path, fname);
-	wr_ioctl.ino = stbuf.st_ino;
-	wr_ioctl.sz = 1;
+	*ino = stbuf.st_ino;
+	return err;
+}
 
-	fd = open(WRAPFS_CDEV, O_RDWR);
+static int do_ioctl(const char *dev, long cmd,
+		    struct wrapfs_misc_ioctl *wr_ioctl)
+{
+	int fd, err;
+
+	fd = open(dev, O_RDWR);
 	if (fd < 0) {
 		printf("open failed: %s\n", strerror(errno));
 		return fd;
 	}
 
-	err = ioctl(fd, cmd, &wr_ioctl);
+	err = ioctl(fd, cmd, wr_ioctl);
 	if (err)
 		printf("ioctl failed: %s\n", strerror(errno));
 
@@ -59,57 +85,115 @@ void trim(char *fname)
 		fname[sz - 1] = '\0';
 }
 
+static int hide_file(char **args)
+{
+	struct wrapfs_misc_ioctl wr_ioctl = {0};
+	int err, cmd;
+	char *dev;
+
+	strcpy(wr_ioctl.path, args[0]);
+	trim(wr_ioctl.path);
+
+	err = get_inode_number(wr_ioctl.path, &wr_ioctl.ino);
+	if (err < 0)
+		return err;
+
+	cmd = WRAPFS_IOC_HIDE;
+	dev = WRAPFS_CDEV;
+
+	return do_ioctl(dev, cmd, &wr_ioctl);
+}
+
+static int unhide_file(char **args)
+{
+	struct wrapfs_misc_ioctl wr_ioctl = {0};
+	int err, cmd;
+	char *dev;
+
+	strcpy(wr_ioctl.path, args[0]);
+	trim(wr_ioctl.path);
+
+	err = get_inode_number(wr_ioctl.path, &wr_ioctl.ino);
+	if (err < 0)
+		return err;
+
+	cmd = WRAPFS_IOC_UNHIDE;
+	dev = WRAPFS_CDEV;
+
+	return do_ioctl(dev, cmd, &wr_ioctl);
+}
+
+static int block_file(char **args)
+{
+	struct wrapfs_misc_ioctl wr_ioctl = {0};
+	int err, cmd;
+	char *dev;
+
+	strcpy(wr_ioctl.path, args[0]);
+	trim(wr_ioctl.path);
+
+	err = get_inode_number(wr_ioctl.path, &wr_ioctl.ino);
+	if (err < 0)
+		return err;
+
+	cmd = WRAPFS_IOC_BLOCK;
+	dev = wr_ioctl.path;
+
+	return do_ioctl(dev, cmd, &wr_ioctl);
+}
+
+static int unblock_file(char **args)
+{
+	struct wrapfs_misc_ioctl wr_ioctl = {0};
+	char *endp;
+	int err, cmd;
+	char *dev;
+
+	strcpy(wr_ioctl.path, args[0]);
+	trim(wr_ioctl.path);
+	wr_ioctl.ino = strtoul(args[1], &endp, 10);
+
+	printf("unblock %s %s\n", args[0], args[1]);
+	cmd = WRAPFS_IOC_UNBLOCK;
+	dev = WRAPFS_CDEV;
+
+	return do_ioctl(dev, cmd, &wr_ioctl);
+}
+
+static int list_all(char **args)
+{
+	struct wrapfs_misc_ioctl wr_ioctl = {0};
+	int err, cmd;
+	char *dev;
+
+	cmd = WRAPFS_IOC_HIDE_LIST;
+	dev = WRAPFS_CDEV;
+	return 0;
+}
+
+static int help(char **args)
+{
+	usage();
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
-	char *fname;
-	int c, err;
+	int min_args = 2, i;
 
-	static const struct option longopts[] = {
-		{ "hide",	required_argument,	0, 'h'},
-		{ "unhide",	required_argument,	0, 'u'},
-		{ "list",	no_argument,		0, 'l'},
-	};
-
+	progname = argv[0];
 	if (argc < 2) {
-		usage(argv);
+		printf("Invalid argurments\n");
+		usage();
 		return -EINVAL;
 	}
 
-	while((c = getopt_long(argc, argv, "h:u:l", longopts, NULL)) != -1) {
-		switch (c){
-		case 'h':
-			fname = strdup(optarg);
-			if (!fname) {
-				printf("failed to allocate\n");
-				return -ENOMEM;
-			}
-			cmd = WRAPFS_IOC_HIDE;
-			break;
-		case 'u':
-			fname = strdup(optarg);
-			if (!fname) {
-				printf("failed to allocate\n");
-				return -ENOMEM;
-			}
-			cmd = WRAPFS_IOC_UNHIDE;
-			break;
-		case 'l':
-			cmd = WRAPFS_IOC_HIDE_LIST;
-			break;
-		default:
-			usage(argv);
-			return -EINVAL;
-		}
+	for (i=0; i<ARRAY_SIZE(cmds); i++) {
+		if (!strcmp(cmds[i].cmd, argv[1]))
+			return cmds[i].func(&argv[2]);
 	}
 
-	if (cmd == 0) {
-		printf("Invalid options used\n");
-		usage(argv);
-		return -EINVAL;
-	}
-
-	trim(fname);
-	err = do_ioctl(fname);
-	free(fname);
-	return err;
+	printf("unknown option\n");
+	usage();
+	return -EINVAL;
 }
